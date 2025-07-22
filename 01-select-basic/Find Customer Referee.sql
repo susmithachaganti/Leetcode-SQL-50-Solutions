b975d646ecd241ff1c7b6387c524b5bfd75f6f15
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-22 11:07:32 +0530

    Find Customer Referee.sql

--- 01-select-basic/Find Customer Referee.sql
+++ 01-select-basic/Find Customer Referee.sql
@@ -1,1 +1,38 @@
+Table: Customer
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+Find the names of the customer that are either:
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+  Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+MYSQL CODE:
+select name from Customer where referee_id IS NULL OR referee_id != 2;
