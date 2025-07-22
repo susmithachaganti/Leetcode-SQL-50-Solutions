09d7a3e5ea2247585bc5af7232b53279ce8a872a
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-22 11:11:42 +0530

    Invalid Tweets.sql

--- 01-select-basic/Invalid Tweets.sql
+++ 01-select-basic/Invalid Tweets.sql
@@ -1,1 +1,27 @@
-
+Table: Tweets
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+MYSQL CODE:
+select tweet_id from Tweets where length(content) > 15;
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet.
