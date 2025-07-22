28162d8671a16f504434517eacc5b69adaebd27e
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-22 11:15:55 +0530

    Recyclable and Low Fat Products.sql

--- 01-select-basic/Recyclable and Low Fat Products.sql
+++ 01-select-basic/Recyclable and Low Fat Products.sql
@@ -8,7 +8,10 @@ Table: Products
 +-------------+---------+
 Question : Write a solution to find the ids of products that are both low fat and recyclable.
 Return the result table in any order.
-Example 1:
+
+MYSQL CODE:
+select product_id from Products  where low_fats = 'Y' and recyclable = 'Y';
+
 Input: 
 Products table:
 +-------------+----------+------------+
@@ -28,5 +31,3 @@ Output:
 | 3           |
 +-------------+
 Explanation: Only products 1 and 3 are both low fat and recyclable.
-CODE:
-select product_id from Products  where low_fats = 'Y' and recyclable = 'Y';
