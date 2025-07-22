82c51c5b6365d91247e75ad741712d19f831e35f
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-22 11:02:01 +0530

    Update Recyclable and Low Fat Products.sql

--- 01-select-basic/Recyclable and Low Fat Products.sql
+++ 01-select-basic/Recyclable and Low Fat Products.sql
@@ -1,1 +1,32 @@
-
+Table: Products
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+Question : Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+Example 1:
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
+CODE:
+select product_id from Products  where low_fats = 'Y' and recyclable = 'Y';
