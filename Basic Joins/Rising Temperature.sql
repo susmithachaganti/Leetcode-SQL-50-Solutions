66f6fa072075edac4f21b952b8b27dbfe68b7f0d
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-23 14:15:33 +0530

    Create Rising Temperature.sql

---
+++ Basic Joins/Rising Temperature.sql
@@ -0,0 +1,33 @@
+Table: Weather
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+MYSQL :
+ SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+  
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
