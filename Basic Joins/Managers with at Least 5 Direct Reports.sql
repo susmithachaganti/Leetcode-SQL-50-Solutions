66ef038599afb66897f8aa825fbd78f583cdb42a
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-25 20:00:11 +0530

    Create Managers with at Least 5 Direct Reports.sql

---
+++ Basic Joins/Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,34 @@
+Table: Employee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+Write a solution to find managers with at least five direct reports.
+  MYSQL CODE :
+**SELECT e.name FROM Employee e
+JOIN(SELECT managerId FROM Employee
+    WHERE managerId IS NOT NULL GROUP BY managerId
+    HAVING COUNT(managerId) >= 5) AS t
+ON e.id = t.managerId;
+Input: 
+Employee table:
++-----+-------+------------+-----------+
+| id  | name  | department | managerId |
++-----+-------+------------+-----------+
+| 101 | John  | A          | null      |
+| 102 | Dan   | A          | 101       |
+| 103 | James | A          | 101       |
+| 104 | Amy   | A          | 101       |
+| 105 | Anne  | A          | 101       |
+| 106 | Ron   | B          | 101       |
++-----+-------+------------+-----------+
+Output: 
++------+
+| name |
++------+
+| John |
++------+
