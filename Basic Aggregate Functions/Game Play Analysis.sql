7a94cd6c622b31f5df69aed04ea13f6cfa00d308
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-07-31 13:54:11 +0530

    Create Game Play Analysis.sql

---
+++ Basic Aggregate Functions/Game Play Analysis.sql
@@ -0,0 +1,31 @@
+  Table: Activity
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to determine the number of players who logged in on the day immediately following their initial login, and divide it by the number of total players.
+  MYSQL CODE:
+     SELECT ROUND( COUNT(DISTINCT T2.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2  ) AS fraction
+    FROM(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) AS T1
+    JOIN Activity AS T2 ON T1.player_id = T2.player_id AND DATEDIFF(T2.event_date, T1.first_login) = 1;
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+
+| fraction  |
++-----------+
+| 0.33      |
++-----------+
