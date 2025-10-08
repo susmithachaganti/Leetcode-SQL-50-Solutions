bffeba374b0031b5d7ac6b83404bbdfca779eacd
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-10-08 11:15:22 +0530

    Create The Number of Employees Which Report to Each Employee.sql

---
+++ 05-Advanced Select and Joins/The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,30 @@
+Table: Employees
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| employee_id | int      |
+| name        | varchar  |
+| reports_to  | int      |
+| age         | int      |
++-------------+----------+
+Write a solution to report the ids and the names of all managers, the number of employees who report directly to them, and the average age of the reports rounded to the nearest integer.
+  MY SQL CODE:
+    SELECT e.employee_id,e.name,COUNT(emp.employee_id) AS reports_count,ROUND(AVG(emp.age)) AS average_age
+    FROM Employees e JOIN Employees emp ON e.employee_id = emp.reports_to
+    GROUP BY e.employee_id, e.name ORDER BY e.employee_id;
+Input: 
+Employees table:
++-------------+---------+------------+-----+
+| employee_id | name    | reports_to | age |
++-------------+---------+------------+-----+
+| 9           | Hercy   | null       | 43  |
+| 6           | Alice   | 9          | 41  |
+| 4           | Bob     | 9          | 36  |
+| 2           | Winston | null       | 37  |
++-------------+---------+------------+-----+
+Output: 
++-------------+-------+---------------+-------------+
+| employee_id | name  | reports_count | average_age |
++-------------+-------+---------------+-------------+
+| 9           | Hercy | 2             | 39          |
++-------------+-------+---------------+-------------+
