aa5a24e7d7de6d5c986024bbd22a3edd4eb7afad
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-10-08 10:45:45 +0530

    Create Average Time of Process per Machine.sql

---
+++ 02-Basic Joins/Average Time of Process per Machine.sql
@@ -0,0 +1,48 @@
+Table: Activity
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| machine_id     | int     |
+| process_id     | int     |
+| activity_type  | enum    |
+| timestamp      | float   |
++----------------+---------+
+
+Write a solution to find the average time each machine takes to complete a process.
+  MY SQL CODE :-
+
+    SELECT machine_id, ROUND(AVG(processing_time), 3) AS processing_time
+    FROM (
+    SELECT machine_id, process_id, MAX(CASE WHEN activity_type = 'end' THEN timestamp END) - MIN(CASE WHEN activity_type = 'start' THEN timestamp END) AS processing_time
+    FROM Activity
+    GROUP BY machine_id, process_id
+    ) AS process_times
+    GROUP BY machine_id;
+
+
+Input: 
+Activity table:
++------------+------------+---------------+-----------+
+| machine_id | process_id | activity_type | timestamp |
++------------+------------+---------------+-----------+
+| 0          | 0          | start         | 0.712     |
+| 0          | 0          | end           | 1.520     |
+| 0          | 1          | start         | 3.140     |
+| 0          | 1          | end           | 4.120     |
+| 1          | 0          | start         | 0.550     |
+| 1          | 0          | end           | 1.550     |
+| 1          | 1          | start         | 0.430     |
+| 1          | 1          | end           | 1.420     |
+| 2          | 0          | start         | 4.100     |
+| 2          | 0          | end           | 4.512     |
+| 2          | 1          | start         | 2.500     |
+| 2          | 1          | end           | 5.000     |
++------------+------------+---------------+-----------+
+Output: 
++------------+-----------------+
+| machine_id | processing_time |
++------------+-----------------+
+| 0          | 0.894           |
+| 1          | 0.995           |
+| 2          | 1.456           |
++------------+-----------------+
