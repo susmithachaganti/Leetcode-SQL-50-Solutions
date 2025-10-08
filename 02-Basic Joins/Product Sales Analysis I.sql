d20609b50ba2b4f254b17aaf2960b9b0079f9b72
susmithachaganti <146943371+susmithachaganti@users.noreply.github.com>
2025-10-08 10:29:29 +0530

    Create Product Sales Analysis I.sql

---
+++ 02-Basic Joins/Product Sales Analysis I.sql
@@ -0,0 +1,48 @@
+Table: Sales
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+Table: Product
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+ MYSQL CODE:
+   
+    select p.product_name,s.year,s.price from Sales s
+    join Product p ON s.product_id = p.product_id;
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
